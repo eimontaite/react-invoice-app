e025da048b82c36fdd04307eb052505bcabdd50d
Aiste <eimontaite.aiste@gmail.com>
2020-05-28 16:45:14 +0300

    Switch from Collapse to Accordion in UserInfoCards view

--- src/components/user-info/UserInfoCard.js
+++ src/components/user-info/UserInfoCard.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import '../../styles/App.css';
-import {Button, ButtonGroup, Collapse, Card} from 'react-bootstrap';
+import {Accordion, Button, ButtonGroup, Collapse, Card} from 'react-bootstrap';
 import Moment from "react-moment";
 
 const UserInfoCard = props => {
@@ -8,31 +8,33 @@ const UserInfoCard = props => {
 
     const userInfoCards = props.userInfo && props.userInfo.map((card, index) => {
         return (
-            <Card style={{ width: '50%' }} bg={"light"}>
-                <Card.Body>
-                    <Card.Title onClick={() => setOpen(!open)}
-                                aria-controls="example-collapse-text"
-                                aria-expanded={open}>
-                        {card.name} {card.surname}, address: {card.address}
-                    </Card.Title>
-                    <Collapse in={open}>
-                    <Card.Text>
-                        <p>Personal Number: {card.personalNumber}</p>
-                        <p>Business Licence No.: {card.businessLicenceNo}</p>
-                        <p>Email: {card.email}</p>
-                        <p>Phone: {card.phone}</p>
-                        <p>Bank Account No.: {card.bankAccount}</p>
-                        <p>Bank: {card.bank}</p>
-                        <p>Created On: { }
-                            <Moment format="YYYY-MM-DD HH:mm">
-                                {card.createDateTime}
-                            </Moment>
-                        </p>
-                    </Card.Text>
-                    </Collapse>
-                    <Button variant="info">Edit</Button>
-                </Card.Body>
-            </Card>
+            <Accordion>
+                <Card style={{ width: '50%' }} bg={"light"}>
+                    <Card.Header>
+                        <Accordion.Toggle as={Card.Header} eventKey={index}>
+                            {card.name} {card.surname}, address: {card.address}
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey={index}>
+                    <Card.Body>
+                            <Card.Text>
+                                <p>Personal Number: {card.personalNumber}</p>
+                                <p>Business Licence No.: {card.businessLicenceNo}</p>
+                                <p>Email: {card.email}</p>
+                                <p>Phone: {card.phone}</p>
+                                <p>Bank Account No.: {card.bankAccount}</p>
+                                <p>Bank: {card.bank}</p>
+                                <p>Created On: { }
+                                    <Moment format="YYYY-MM-DD HH:mm">
+                                        {card.createDateTime}
+                                    </Moment>
+                                </p>
+                            </Card.Text>
+                        <Button variant="info">Edit</Button>
+                    </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion>
         )
     }
     );
--- src/components/user-info/UserInfoForm.js
+++ src/components/user-info/UserInfoForm.js
@@ -1,63 +1,59 @@
 import React, {useState} from 'react';
 import {Button, Form} from "react-bootstrap";
 
-const UserInfoForm = () => {
-    const [validated, setValidated] = useState(false);
+const UserInfoForm = props => {
 
-    const handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-
-        setValidated(true);
-    };
     return (
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={props.handleSubmit} onChange={props.myChangeHandler}>
+            {/*todo use actual userId*/}
+            <Form.Group disabled controlId="formUserId">
+                <Form.Label>User ID</Form.Label>
+                <Form.Control name="userId" type="number" value="2"/>
+            </Form.Group>
+
             <Form.Group controlId="formName">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="name" placeholder="Name"/>
+                <Form.Control required name="name" type="input" placeholder="Name"/>
             </Form.Group>
 
             <Form.Group controlId="formSurname">
                 <Form.Label>Surname</Form.Label>
-                <Form.Control type="surname" placeholder="Surname"/>
+                <Form.Control required name="surname" type="input" placeholder="Surname"/>
             </Form.Group>
 
             <Form.Group controlId="formPersonalNumber">
                 <Form.Label>Personal Number</Form.Label>
-                <Form.Control type="personalNumber" placeholder="Your personal number (optional)"/>
+                <Form.Control name="personalNumber" type="input" placeholder="Your personal number (optional)"/>
             </Form.Group>
 
             <Form.Group controlId="formBusinessLicenceNo">
                 <Form.Label>Business Licence No.</Form.Label>
-                <Form.Control type="businessLicenceNo" placeholder="Your business licence number"/>
+                <Form.Control required name="businessLicenceNo" type="input" placeholder="Your business licence number"/>
             </Form.Group>
 
             <Form.Group controlId="formAddress">
                 <Form.Label>Address</Form.Label>
-                <Form.Control type="address" placeholder="Your address"/>
+                <Form.Control required name="address" type="input" placeholder="Your address"/>
             </Form.Group>
 
             <Form.Group controlId="formEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Your email"/>
+                <Form.Control name="email" type="input" placeholder="Your email"/>
             </Form.Group>
 
             <Form.Group controlId="formPhone">
                 <Form.Label>Phone</Form.Label>
-                <Form.Control type="phone" placeholder="Phone"/>
+                <Form.Control name="phone" type="input" placeholder="Phone"/>
             </Form.Group>
 
             <Form.Group controlId="formBankAccountNo">
                 <Form.Label>Bank Account No.</Form.Label>
-                <Form.Control type="bankAccount" placeholder="Your bank account number"/>
+                <Form.Control required name="bankAccount" type="input" placeholder="Your bank account number"/>
             </Form.Group>
 
             <Form.Group controlId="formBank">
                 <Form.Label>Bank</Form.Label>
-                <Form.Control type="bank" placeholder="Bank"/>
+                <Form.Control required name="bank" type="input" placeholder="Bank"/>
             </Form.Group>
 
             <Button variant="primary" type="submit">
